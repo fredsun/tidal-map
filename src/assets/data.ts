4bfcf0b79f5d9818c1d6bcd3952b781c1b14f4c4
fredsun <fredsun.me@gmail.com>
2023-04-07 21:31:39 +0800

    feat: markrt点击popup后显示echart

---
+++ src/assets/data.ts
@@ -0,0 +1,137 @@
+var bicycleRental = {
+    "type": "FeatureConllection",
+    "features": [
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [118.8, 32.05],
+                "properties": {
+                    "popupContent": "南京",
+                    "idStr": "njChart"
+                }
+            },
+            "type": "Feature",
+            "id": 100
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [119.2, 34.55],
+                "properties": {
+                    "popupContent": "连云港",
+                    "idStr": "lygChart"
+                }
+            },
+            "type": "Feature",
+            "id": 101
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [118.26, 33.94],
+                "properties": {
+                    "popupContent": "宿迁",
+                    "idStr": "sqChart"
+                }
+            },
+            "type": "Feature",
+            "id": 102
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [119.12, 33.53],
+                "properties": {
+                    "popupContent": "淮安",
+                    "idStr": "haChart"
+                }
+            },
+            "type": "Feature",
+            "id": 103
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [120.15, 33.32],
+                "properties": {
+                    "popupContent": "盐城",
+                    "idStr": "ycChart"
+                }
+            },
+            "type": "Feature",
+            "id": 104
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [119.95, 32.40],
+                "properties": {
+                    "popupContent": "泰州",
+                    "idStr": "tzChart"
+                }
+            },
+            "type": "Feature",
+            "id": 105
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [119.41, 32.35],
+                "properties": {
+                    "popupContent": "扬州",
+                    "idStr": "yzChart"
+                }
+            },
+            "type": "Feature",
+            "id": 106
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [120.92, 31.94],
+                "properties": {
+                    "popupContent": "南通",
+                    "idStr": "ntChart"
+                }
+            },
+            "type": "Feature",
+            "id": 107
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [119.99, 31.755],
+                "properties": {
+                    "popupContent": "常州",
+                    "idStr": "czChart"
+                }
+            },
+            "type": "Feature",
+            "id": 108
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [120.33, 31.44],
+                "properties": {
+                    "popupContent": "无锡",
+                    "idStr": "wxChart"
+                }
+            },
+            "type": "Feature",
+            "id": 109
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [120.61, 31.26],
+                "properties": {
+                    "popupContent": "苏州",
+                    "idStr": "szChart"
+                }
+            },
+            "type": "Feature",
+            "id": 110
+        }
+    ]
+}
--- src/layouts/components/Map/index.vue
+++ src/layouts/components/Map/index.vue
@@ -7,6 +7,7 @@ import 'leaflet/dist/leaflet.css'
 import { onMounted, defineProps, watch, inject, ref } from 'vue'
 import { getMapKeystore } from '@/stores/keystore'
 import * as echarts from 'echarts';
+import '@/assets/data.ts'
 
 var map;
 onMounted(() => {
@@ -18,34 +19,67 @@ function initMap() {
     const cvaLayer = L.tileLayer(`https://t0.tianditu.gov.cn/cva_w/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=cva&STYLE=default&TILEMATRIXSET=w&FORMAT=tiles&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}&tk=${getMapKeystore()}`);
     const imgLayer = L.tileLayer(`https://t0.tianditu.gov.cn/img_w/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=img&STYLE=default&TILEMATRIXSET=w&FORMAT=tiles&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}&tk=${getMapKeystore()}`);
     const ciaLayer = L.tileLayer(`https://t0.tianditu.gov.cn/cia_w/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=cia&STYLE=default&TILEMATRIXSET=w&FORMAT=tiles&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}&tk=${getMapKeystore()}`);
-     //矢量图层组
+    //矢量图层组
     const vecLayerGroup = L.layerGroup([vecLayer, cvaLayer])
-     //影像图层组
+    //影像图层组
     const imgLayerGroup = L.layerGroup([imgLayer, ciaLayer])
     var baseLayers = {
-    "天地图矢量": vecLayerGroup,
-    "天地图影像": imgLayerGroup
-  };
-  let centerLatlng = [32.063417, 118.849672];
-  map = L.map('myMap', {  //需绑定地图容器div的id
-    center: [32.659122,114.311576], //初始地图中心
-    zoom: 13, //初始缩放等级
-    maxZoom: 18, //最大缩放等级
-    minZoom: 0, //最小缩放等级
-    zoomControl: false,//不显示缩放小控件
-    layers: [vecLayerGroup]
-  });
-  var popup = L.popup({
-    keepInView: true,
-    closeButton: false,
-    autoPan:false
-  })
-    .setLatLng(centerLatlng)
-    .setContent('<p>Hello world!<br />This is a nice popup.</p>');
-    
-  L.marker([32.063417, 118.849672]).addTo(map)
-    .bindPopup(popup)
-    ;
+        "天地图矢量": vecLayerGroup,
+        "天地图影像": imgLayerGroup
+    };
+    let centerLatlng = [32.063417, 118.849672];
+    map = L.map('myMap', {  //需绑定地图容器div的id
+        center: [32.659122, 114.311576], //初始地图中心
+        zoom: 13, //初始缩放等级
+        maxZoom: 18, //最大缩放等级
+        minZoom: 0, //最小缩放等级
+        zoomControl: false,//不显示缩放小控件
+        layers: [vecLayerGroup]
+    });
+    var popup = L.popup({
+        keepInView: true,
+        closeButton: false,
+        autoPan: false
+    })
+        .setLatLng(centerLatlng)
+        .setContent('<p>Hello world!<br />This is a nice popup.</p>');
+
+        
+    var idStr = "aaa";
+    var popupContent = '<div style="width:350px;height:300px" id="' + idStr + '"></div>';
+    L.marker([32.063417, 118.849672]).addTo(map)
+        .bindPopup(popupContent)
+        .on('popupopen', function (e) {
+
+            var myChart = echarts.init(document.getElementById(idStr));
+            // 指定图表的配置项和数据
+            var option = {
+                title: {
+                    text: '测试'
+                },
+                tooltip: {},
+                legend: {
+                    data: ['销量']
+                },
+                xAxis: {
+                    data: ["衬衫", "羊毛衫", "雪纺衫", "裤子"]
+                },
+                yAxis: {},
+                series: [{
+                    name: '销量',
+                    type: 'bar',
+                    data: [5, 20, 36, 10]
+                }]
+            };
+
+            // 使用刚指定的配置项和数据显示图表。
+            myChart.setOption(option);
+        })
+        ;
+
+
+
+
 }
 
 </script>
