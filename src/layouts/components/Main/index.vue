6af7db5c2a6335e8adab4f16da1407694fc1efe0
fredsun <fredsun.me@gmail.com>
2023-04-07 10:45:10 +0800

    feat tsconfig补充基于src的模块路径查找

---
+++ src/layouts/components/Main/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <el-container>
+        <el-header>Header</el-header>
+        <el-main>
+            <Map></Map>
+        </el-main>
+    </el-container>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import Map from "@/layouts/components/Map/index.vue";
+</script>
